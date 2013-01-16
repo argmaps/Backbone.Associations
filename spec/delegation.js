a0d9c8d73cf6a2b0443f5a74f4fcff8ce4c3362a
Josh Weinstein <weinstein.josh@gmail.com>
2013-01-15 20:53:43 -0500

    Re-organize delegation specs.

--- spec/delegation.js
+++ spec/delegation.js
@@ -14,12 +14,12 @@ describe("`delegateAttributes` property, for delegating attributes on instantiat
             }
         });
 
-        this.delegate = new DelegateModel();
+        this.delegateModel = new DelegateModel();
     });
 
     it("does not set the attribute directly on the delegating model", function() {
         this.delegating = new ModelWithDelegatedAttr({
-            delegateModel: this.delegate,
+            delegateModel: this.delegateModel,
             delegatedAttrName: 'Yes'
         });
         expect(this.delegating.attributes.delegatedAttrName).not.toBeDefined();
@@ -43,8 +43,160 @@ describe("`delegateAttributes` property, for delegating attributes on instantiat
         };
 
         this.delegating = new ModelWithDelegatedAttr({
-            delegateModel: this.delegate,
+            delegateModel: this.delegateModel,
             delegatedAttrName: 'Yes'
+        });
+    });
+
+    describe("basic features", function() {
+        beforeEach(function() {
+            this.subject = new ModelWithDelegatedAttr({delegateModel: this.delegateModel});
+        });
+
+        it("sets the attribute value on the delegate model", function() {
+            this.subject.set({"delegatedAttrName": 22});
+            expect(this.subject.get("delegatedAttrName")).toEqual(22);
+        });
+
+        it("lets the model with delegated attr retrieve the value of the delegated attr", function() {
+            this.subject.set({"delegatedAttrName": 22});
+            expect(this.subject.get("delegatedAttrName")).toEqual(22);
+        });
+
+        it("when {validate: true}, is passed, it calls #validate on the model with delegated attr prior to setting on the delegate model", function() {
+            this.subject.validate = function() {};
+            spyOn(this.subject, 'validate');
+            this.subject.set({"delegatedAttrName": 22}, {validate: true});
+            expect(this.subject.validate).wasCalled();
+        });
+
+        it("when a delegated attr is set on the delegating model, a change:attrName event fires on the delegating model", function() {
+            var callback = jasmine.createSpy();
+
+            this.subject.on('change:delegatedAttrName', callback, this.subject);
+            this.subject.set('delegatedAttrName', 'changed', {myOption: true});
+            this.subject.off('change:delegatedAttrName', callback, this.subject); //necessary otherwise later tests fail
+            expect(callback).toHaveBeenCalledWith(this.delegateModel, 'changed', {myOption: true});
+        });
+
+        it("when a delegated attr is set on the model to which it is delegated, a change:attrName event fires on the delegating model", function() {
+            var callback = jasmine.createSpy();
+
+            this.subject.on('change:delegatedAttrName', callback, this.subject);
+            this.delegateModel.set('delegatedAttrName', 'changed', {myOption: true});
+            this.subject.off('change:delegatedAttrName', callback, this.subject); //necessary otherwise later tests fail
+            expect(callback).toHaveBeenCalledWith(this.delegateModel, 'changed', {myOption: true});
+        });
+
+        it("when a change:attrName is triggered on the delegate model, a change:attrName event fires on the delegating model", function() {
+            var callback = jasmine.createSpy();
+
+            this.subject.on('change:delegatedAttrName', callback, this.subject);
+            this.delegateModel.trigger('change:delegatedAttrName', this.delegateModel, 'changed');
+            this.subject.off('change:delegatedAttrName', callback, this.subject); //necessary otherwise later tests fail
+            expect(callback).toHaveBeenCalledWith(this.delegateModel, 'changed', undefined);
+        });
+
+        it("modelWithDelegatedAttr#previous(delegatedAttrName) does not return the previous value of the delegatedAttr, but the delegateModel argument does", function() {
+            this.testPreviousCallBack = function(delegateModel, value, options){
+                expect(this.previous('delegatedAttrName') === 'original').toBeFalsy();
+                expect(delegateModel.previous('delegatedAttrName') === 'original').toBeTruthy();
+            };
+
+            this.subject.set('delegatedAttrName', 'original');
+            this.subject.on('change:delegatedAttrName', this.testPreviousCallBack, this.subject);
+            this.subject.set('delegatedAttrName', 'changed');
+            this.subject.off('change:delegatedAttrName', this.testPreviousCallBack, this.subject); //necessary otherwise later tests fail
+        });
+    });
+
+    describe("when an attribute is delegated that also has a default specified", function() {
+        beforeEach(function() {
+            DelegateModel = Backbone.AssociativeModel.extend({
+                associations: function() {
+                    this.hasOne('modelWithDelegatedAttr').viaReverseKey('delegateModel');
+                }
+            });
+
+            ModelWithDelegatedAttr = Backbone.AssociativeModel.extend({
+                defaults: function() {
+                    return {
+                        delegatedAttrName: 23
+                    };
+                },
+
+                associations: function() {  this.hasOne('delegateModel');  },
+
+                delegateAttributes: {
+                    'delegatedAttrName': 'delegateModel'
+                }
+            });
+
+            this.delegate = new DelegateModel();
+            this.delegating = new ModelWithDelegatedAttr({
+                delegateModel: this.delegate
+            });
+        });
+
+        it("sets the delegated attribute to the default value on the delegate model", function() {
+            expect(this.delegate.get('delegatedAttrName')).toEqual(23);
+        });
+
+        it("removes the delegated attribute and its default value from the delegating model's attributes", function() {
+            expect(this.delegating.attributes.delegatedAttrName).not.toBeDefined();
+        });
+    });
+
+    describe("delegating multiple attributes to the same delegate model", function() {
+        beforeEach(function() {
+            DelegateModel = Backbone.AssociativeModel.extend({});
+
+            ModelWithDelegatedAttr = Backbone.AssociativeModel.extend({
+                associations: function() {  this.hasOne('delegateModel');  },
+                delegateAttributes: {
+                    'delegatedAttrName1 delegatedAttrName2': 'delegateModel'
+                }
+            });
+
+            this.delegateModel = new DelegateModel();
+            this.subject = new ModelWithDelegatedAttr().set({delegateModel: this.delegateModel});
+        });
+
+        it("sets the delegated attribute values on the delegate model", function() {
+            this.subject.set({
+                delegatedAttrName1: 300,
+                delegatedAttrName2: 400
+            });
+
+            expect(this.delegateModel.get('delegatedAttrName1')).toEqual(300);
+            expect(this.delegateModel.get('delegatedAttrName2')).toEqual(400);
+        });
+
+        it("retrieves the delegated attributes from the delegatING model", function() {
+            this.delegateModel.set({ delegatedAttrName1: 300 });
+            expect(this.subject.get('delegatedAttrName1')).toEqual(300);
+        });
+
+        describe("when the delegate model is destroyed", function() {
+            it("returns undefined for attributes delegated to the destroyed delegate model", function() {
+                this.delegateModel.set({ delegatedAttrName1: 300 });
+                this.delegateModel.destroy();
+                expect(this.subject.get('delegatedAttrName1')).toBeUndefined();
+            });
+        });
+
+        describe("when a different delegate model is set for the same delegated model attribute", function() {
+            it("the delegatING model retrieves delegated attrs from the new delegate model", function() {
+                //first delegate model
+                this.delegateModel.set({ delegatedAttrName1: 300 });
+                expect(this.subject.get('delegatedAttrName1')).toEqual(300);
+
+
+                //second delegate model
+                var secondDelegateModel = new DelegateModel({delegatedAttrName1: 500});
+                this.subject.set({delegateModel: secondDelegateModel});
+                expect(this.subject.get('delegatedAttrName1')).toEqual(500);
+            });
         });
     });
 });
@@ -128,172 +280,5 @@ describe("`delegateMethods` property", function() {
 
         expect(this.delegateModel.delegatedMethod1).toHaveBeenCalledWith('yo1');
         expect(this.delegateModel.delegatedMethod2).toHaveBeenCalledWith('yo2');
-    });
-});
-
-describe("#delegateAttributesInternal", function() {
-    beforeEach(function() {
-        DelegateModel = Backbone.AssociativeModel.extend({});
-
-        ModelWithDelegatedAttr = Backbone.AssociativeModel.extend({
-            associations: function() {  this.hasOne('delegateModel');  },
-            delegateAttributes: {
-                'delegatedAttrName1 delegatedAttrName2': 'delegateModel'
-            }
-        });
-
-        this.delegateModel = new DelegateModel();
-        this.subject = new ModelWithDelegatedAttr().set({delegateModel: this.delegateModel});
-    });
-
-    it("sets the delegated attribute values on the delegate model", function() {
-        this.subject.set({
-            delegatedAttrName1: 300,
-            delegatedAttrName2: 400
-        });
-
-        expect(this.delegateModel.get('delegatedAttrName1')).toEqual(300);
-        expect(this.delegateModel.get('delegatedAttrName2')).toEqual(400);
-    });
-
-    it("retrieves the delegated attributes from the delegatING model", function() {
-        this.delegateModel.set({ delegatedAttrName1: 300 });
-        expect(this.subject.get('delegatedAttrName1')).toEqual(300);
-    });
-
-    describe("when the delegate model is destroyed", function() {
-        it("returns undefined for attributes delegated to the destroyed delegate model", function() {
-            this.delegateModel.set({ delegatedAttrName1: 300 });
-            this.delegateModel.destroy();
-            expect(this.subject.get('delegatedAttrName1')).toBeUndefined();
-        });
-    });
-
-    describe("when a different delegate model is set for the same delegated model attribute", function() {
-        it("the delegatING model retrieves delegated attrs from the new delegate model", function() {
-            //first delegate model
-            this.delegateModel.set({ delegatedAttrName1: 300 });
-            expect(this.subject.get('delegatedAttrName1')).toEqual(300);
-
-
-            //second delegate model
-            var secondDelegateModel = new DelegateModel({delegatedAttrName1: 500});
-            this.subject.set({delegateModel: secondDelegateModel});
-            expect(this.subject.get('delegatedAttrName1')).toEqual(500);
-        });
-    });
-});
-
-describe("#delegateAttribute", function() {
-    beforeEach(function() {
-        DelegateModel = Backbone.AssociativeModel.extend({
-            associations: function() {
-                this.hasOne('modelWithDelegatedAttr').viaReverseKey('delegateModel');
-            }
-        });
-
-        ModelWithDelegatedAttr = Backbone.AssociativeModel.extend({
-            associations: function() {  this.hasOne('delegateModel');  },
-
-            delegateAttributes: {
-                'delegatedAttrName': 'delegateModel'
-            }
-        });
-
-        this.delegateModel = new DelegateModel();
-        this.subject = new ModelWithDelegatedAttr({delegateModel: this.delegateModel});
-    });
-
-    it("sets the attribute value on the delegate model", function() {
-        this.subject.set({"delegatedAttrName": 22});
-        expect(this.subject.get("delegatedAttrName")).toEqual(22);
-    });
-
-    it("lets the model with delegated attr retrieve the value of the delegated attr", function() {
-        this.subject.set({"delegatedAttrName": 22});
-        expect(this.subject.get("delegatedAttrName")).toEqual(22);
-    });
-
-    it("when {validate: true}, is passed, it calls #validate on the model with delegated attr prior to setting on the delegate model", function() {
-        this.subject.validate = function() {};
-        spyOn(this.subject, 'validate');
-        this.subject.set({"delegatedAttrName": 22}, {validate: true});
-        expect(this.subject.validate).wasCalled();
-    });
-
-    it("when a delegated attr is set on the delegating model, a change:attrName event fires on the delegating model", function() {
-        var callback = jasmine.createSpy();
-
-        this.subject.on('change:delegatedAttrName', callback, this.subject);
-        this.subject.set('delegatedAttrName', 'changed', {myOption: true});
-        this.subject.off('change:delegatedAttrName', callback, this.subject); //necessary otherwise later tests fail
-        expect(callback).toHaveBeenCalledWith(this.delegateModel, 'changed', {myOption: true});
-    });
-
-    it("when a delegated attr is set on the model to which it is delegated, a change:attrName event fires on the delegating model", function() {
-        var callback = jasmine.createSpy();
-
-        this.subject.on('change:delegatedAttrName', callback, this.subject);
-        this.delegateModel.set('delegatedAttrName', 'changed', {myOption: true});
-        this.subject.off('change:delegatedAttrName', callback, this.subject); //necessary otherwise later tests fail
-        expect(callback).toHaveBeenCalledWith(this.delegateModel, 'changed', {myOption: true});
-    });
-
-    it("when a change:attrName is triggered on the delegate model, a change:attrName event fires on the delegating model", function() {
-        var callback = jasmine.createSpy();
-
-        this.subject.on('change:delegatedAttrName', callback, this.subject);
-        this.delegateModel.trigger('change:delegatedAttrName', this.delegateModel, 'changed');
-        this.subject.off('change:delegatedAttrName', callback, this.subject); //necessary otherwise later tests fail
-        expect(callback).toHaveBeenCalledWith(this.delegateModel, 'changed', undefined);
-    });
-
-    it("modelWithDelegatedAttr#previous(delegatedAttrName) does not return the previous value of the delegatedAttr, but the delegateModel argument does", function() {
-        this.testPreviousCallBack = function(delegateModel, value, options){
-            expect(this.previous('delegatedAttrName') === 'original').toBeFalsy();
-            expect(delegateModel.previous('delegatedAttrName') === 'original').toBeTruthy();
-        };
-
-        this.subject.set('delegatedAttrName', 'original');
-        this.subject.on('change:delegatedAttrName', this.testPreviousCallBack, this.subject);
-        this.subject.set('delegatedAttrName', 'changed');
-        this.subject.off('change:delegatedAttrName', this.testPreviousCallBack, this.subject); //necessary otherwise later tests fail
-    });
-});
-
-describe("when an attribute is delegated that also has a default specified", function() {
-    beforeEach(function() {
-        DelegateModel = Backbone.AssociativeModel.extend({
-            associations: function() {
-                this.hasOne('modelWithDelegatedAttr').viaReverseKey('delegateModel');
-            }
-        });
-
-        ModelWithDelegatedAttr = Backbone.AssociativeModel.extend({
-            defaults: function() {
-                return {
-                    delegatedAttrName: 23
-                };
-            },
-
-            associations: function() {  this.hasOne('delegateModel');  },
-
-            delegateAttributes: {
-                'delegatedAttrName': 'delegateModel'
-            }
-        });
-
-        this.delegate = new DelegateModel();
-        this.delegating = new ModelWithDelegatedAttr({
-            delegateModel: this.delegate
-        });
-    });
-
-    it("sets the delegated attribute to the default value on the delegate model", function() {
-        expect(this.delegate.get('delegatedAttrName')).toEqual(23);
-    });
-
-    it("removes the delegated attribute and its default value from the delegating model's attributes", function() {
-        expect(this.delegating.attributes.delegatedAttrName).not.toBeDefined();
     });
 });
